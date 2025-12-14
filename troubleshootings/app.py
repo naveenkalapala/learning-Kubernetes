a8d09620684d5b45a5beb1fcce5177f726ce4507
Naveen Kalapala <naveenkalapala1252@gmail.com>
2025-12-14 22:07:26 +0530

    flask

--- troubleshootings/app.py
+++ troubleshootings/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello, Guys! This is Flask App! for crashloop testing by Naveen Kalapala'
+    return 'Hello, Guys! This is Flask App! for Crashloop/LivenessProbe testing by Naveen Kalapala'
+
+@app.route('/health')
+def health():
+    return 'Im Healthy! Don`t Worry!', 200
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
